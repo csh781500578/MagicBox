27b5c048aba36b2a15753207a8cfd64e2374e0d4
hanrychen <781500578@qq.com>
2020-02-18 20:19:37 +0800

    初始代码

---
+++ Classes/NBTools.swift
@@ -0,0 +1,20 @@
+//
+//  NBTools.swift
+//  NiceBeauty-Swift
+//
+//  Created by 陈思行 on 2019/9/1.
+//  Copyright © 2019 陈思行. All rights reserved.
+//
+
+import UIKit
+
+class NBTools: NSObject {
+
+    
+    static func isValidity(_ string: String?) -> Bool {
+        if let str = string, str.count > 0 {
+            return true
+        }
+        return false
+    }
+}
